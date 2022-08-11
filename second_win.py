4af33d462624eb78e827bd883090a9f23dcf0363
matvam2005 <110895400+matvam2005@users.noreply.github.com>
2022-08-11 12:24:56 +0300

    Добавил проект

---
+++ second_win.py
@@ -0,0 +1,7 @@
+# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
